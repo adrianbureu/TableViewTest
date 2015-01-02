003a7ab8cedc2eea56092fd7a3cd34761a43f673
fxsnowy <adribure@msn.com>
2015-01-02 18:15:13 -0500

    Initial Commit

---
+++ Table View Test/ABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABAppDelegate.h
+//  Table View Test
+//
+//  Created by Adrian Bureu on 1/2/15.
+//  Copyright (c) 2015 Adrian Bureu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Table View Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Table View Test
+//
+//  Created by Adrian Bureu on 1/2/15.
+//  Copyright (c) 2015 Adrian Bureu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ABAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ABAppDelegate class]));
+    }
+}
